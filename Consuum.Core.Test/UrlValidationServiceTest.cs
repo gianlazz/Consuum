8a04e11bc6ead1100a8487c5be65afbe1d7ebd2a
Gian Lazzarini <gianlazzarini@gmail.com>
2018-03-24 16:52:37 -0700

    Now have 5 passing and one failing test against UrlValidationService.CheckForUrls which performs acceptably for now in returning total urls

--- Consuum.Core.Test/UrlValidationServiceTest.cs
+++ Consuum.Core.Test/UrlValidationServiceTest.cs
@@ -11,9 +11,12 @@ namespace Consuum.Core.Test
     {
         #region Testing Consts
         //Simple Url Variables
-        private const string SingleValidUrl = "http://google.com";
-        private const string SingleInvalidUrl = "http://asdfasljasdkjlfakjhaskjhkhjaskjhfkjkaskhjhkja.com";
+        private const string ValidHttpLink = "http://google.com";
+        private const string ValidHttpsLink = "https://google.com";
+        private const string ValidWwwLink = "www.google.com";
+        private const string ValidLinkWithoutPrefix = "google.com";
         //Moderately Challenging Url Variables
+        private const string SingleInvalidUrl = "http://asdfasljasdkjlfakjhaskjhkhjaskjhfkjkaskhjhkja.com";
         private const string ValidUrls = "http://google.com google.com www.google.com https://google.com";
         private const string InvalidUrls = ""; //partial urls or urls that don't resolve to real sites
         //Complex Url Variables
@@ -24,46 +27,109 @@ namespace Consuum.Core.Test
         #endregion
 
         [Test]
-        public void CheckForUrls_AcceptsString_ReturnsIntToSignifyFoundUrls()
+        public void CheckForUrls_Pass1ValidHttpLink_FindsAndReturnsIntCountOf1()
         {
             //Arrange
             UrlValidationService urlValidationService = new UrlValidationService();
 
             //Act
-            int results = urlValidationService.CheckForUrls("and example of ocr text that contains urls");
+            int results = urlValidationService.CheckForUrls(ValidHttpLink);
 
             //Assert
-            Assert.That(results <= 0);
-
+            Assert.AreEqual(1, results);
         }
 
         [Test]
-        public void ParseForUrls_AcceptsString_ReturnsPossibleUrls()
+        public void CheckForUrls_Pass1ValidHttpsLink_FindsAndReturnsIntCountOf1()
         {
             //Arrange
             UrlValidationService urlValidationService = new UrlValidationService();
 
             //Act
-            urlValidationService.ParseForUrls("and example of ocr text that contains urls");
+            int results = urlValidationService.CheckForUrls(ValidHttpsLink);
 
             //Assert
-
+            Assert.AreEqual(1, results);
         }
 
         [Test]
-        public void ValidateUrl_ValidUrl_ShouldReturnUrlMarkedAsInvalid()
+        public void CheckForUrls_PassValidWwwLink_FindsAndReturnsIntCountOf1()
         {
+            //Arrange
+            UrlValidationService urlValidationService = new UrlValidationService();
 
+            //Act
+            int results = urlValidationService.CheckForUrls(ValidWwwLink);
+
+            //Assert
+            Assert.AreEqual(1, results);
         }
 
         [Test]
-        /* I imagine this using google or some kind of third party search service which will probably
-         * require an interface and dependency injection to fulfill so I'm not bound by specific solutions.
-         */
-        
-        public void AttemptToFixPartialUrls_AcceptsListOfUrls_ReturnsIntToSignifyHowManyWereFixed()
+        public void CheckForUrls_ValidLinkWithoutPrefix_FindsAndReturnsIntCountOf1()
         {
+            //Arrange
+            UrlValidationService urlValidationService = new UrlValidationService();
 
+            //Act
+            int results = urlValidationService.CheckForUrls(ValidLinkWithoutPrefix);
+
+            //Assert
+            Assert.AreEqual(1, results);
         }
+
+        [Test]
+        public void CheckForUrls_AllSimpleUrls_FindsAndReturnsIntCountOf_4()
+        {
+            //Arrange
+            UrlValidationService urlValidationService = new UrlValidationService();
+
+            //Act
+            int results = urlValidationService.CheckForUrls($"{ValidHttpLink} {ValidHttpsLink} {ValidWwwLink} {ValidLinkWithoutPrefix}");
+
+            //Assert
+            Assert.AreEqual(4, results);
+        }
+
+        [Test]
+        public void CheckForUrls_AllSimpleUrlsWithoutSpaces_FindsAndReturnsIntCountOf_4()
+        {
+            //Arrange
+            UrlValidationService urlValidationService = new UrlValidationService();
+
+            //Act
+            int results = urlValidationService.CheckForUrls($"{ValidHttpLink}{ValidHttpsLink}{ValidWwwLink}{ValidLinkWithoutPrefix}");
+
+            //Assert
+            Assert.AreEqual(4, results);
+        }
+        //[Test]
+        //public void ParseForUrls_AcceptsString_ReturnsPossibleUrls()
+        //{
+        //    //Arrange
+        //    UrlValidationService urlValidationService = new UrlValidationService();
+
+        //    //Act
+        //    urlValidationService.ParseForUrls("and example of ocr text that contains urls");
+
+        //    //Assert
+
+        //}
+
+        //[Test]
+        //public void ValidateUrl_ValidUrl_ShouldReturnUrlMarkedAsInvalid()
+        //{
+
+        //}
+
+        //[Test]
+        ///* I imagine this using google or some kind of third party search service which will probably
+        // * require an interface and dependency injection to fulfill so I'm not bound by specific solutions.
+        // */
+
+        //public void AttemptToFixPartialUrls_AcceptsListOfUrls_ReturnsIntToSignifyHowManyWereFixed()
+        //{
+
+        //}
     }
 }
--- Consuum.Core/Services/UrlValidationService.cs
+++ Consuum.Core/Services/UrlValidationService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 using Consuum.Core.Models;
 
 namespace Consuum.Core.Services
@@ -12,9 +13,42 @@ namespace Consuum.Core.Services
             return false;
         }
 
-        public int CheckForUrls(string v)
+        public int CheckForUrls(string input)
         {
-            throw new NotImplementedException();
+            //var text = Regex.Replace(input,
+            //    @"((http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?)",
+            //    "<a target='_blank' href='$1'>$1</a>");
+
+            /* Loop through the regex output
+             * and for each url increment the cound
+             * then return that result or else return 0
+             */
+            //return 0;
+
+            //This one doesn't catch https
+            //Regex regx = new Regex("http://([\\w+?\\.\\w+])+([a-zA-Z0-9\\~\\!\\@\\#\\$\\%\\^\\&amp;\\*\\(\\)_\\-\\=\\+\\\\\\/\\?\\.\\:\\;\\'\\,]*)?", RegexOptions.IgnoreCase);
+
+            //This one finds http and https well!
+            //Regex regx = new Regex(@"((http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?)", RegexOptions.IgnoreCase);
+
+            //This one seems perfect but I cant get it to work
+            //Regex regx = new Regex(@"((http:\/\/ www\.| https:\/\/ www\.| http:\/\/| https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})? (\/.*)?)", RegexOptions.IgnoreCase);
+            //^ (http:\/\/ www\.| https:\/\/ www\.| http:\/\/| https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})? (\/.*)?$
+
+            Regex regx = new Regex(@"(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-?=%.]+", RegexOptions.IgnoreCase);
+
+
+            MatchCollection mactches = regx.Matches(input);
+
+            List<string> urls = new List<string>();
+
+            foreach (Match match in mactches)
+            {
+                //input = input.Replace(match.Value, "<a href='" + match.Value + "'>" + match.Value + "</a>");
+                urls.Add(match.ToString());
+            }
+
+            return urls.Count;
         }
     }
 }
