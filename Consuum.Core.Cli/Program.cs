8b83ed37dbfe82e22017ea435a4401c1b2d29992
Gian Lazzarini <gianlazzarini@gmail.com>
2018-03-24 17:21:28 -0700

    Added basic manual testing .net core Cli layer that tests the UrlValidationService.CheckForurls method

---
+++ Consuum.Core.Cli/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Consuum.Core.Interfaces;
+using Consuum.Core.Models;
+using Consuum.Core.Services;
+
+namespace Consuum.Core.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter in a string that contains urls: ");
+            UrlValidationService urlValidation = new UrlValidationService();
+            Console.WriteLine($"{urlValidation.CheckForUrls(Console.ReadLine())} urls found.");
+
+            Console.ReadKey();
+        }
+    }
+}
