c9dc039caa225a3791d137a1d59cb4c879f2e578
Gian Lazzarini <gianlazzarini@gmail.com>
2018-03-25 19:05:26 -0700

    Added notes to the textannotation class and mocked a winforms desktop gui

--- Consuum.Core/Models/TextAnnotation.cs
+++ Consuum.Core/Models/TextAnnotation.cs
@@ -6,6 +6,16 @@ namespace Consuum.Core.Models
 {
     public class TextAnnotation
     {
+        /* Should ultimately organize content into
+         * pages, paragraphs, and words. All of which
+         * should retain x&y coordinates if att all possible
+         * for re-rendering.
+         * 
+         * It would be nice to also have the associated Image
+         * object from which it was created. That way I could
+         * overlay the words scanned over the original image
+         * for cool animations or renderings of the reading process.
+         */
         public string Text { get; set; }
         public List<Url> ContainedLinks { get; set; }
     }
