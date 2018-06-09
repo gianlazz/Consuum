740f621483c6cff129d6ac3c5a10f49d425bbd98
Gian Lazzarini <gianlazzarini@gmail.com>
2018-06-09 16:18:29 -0700

    Created a clipboard layer project and added references

---
+++ Consuum.Clipboard/MacClipboard.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Consuum.Domain;
+
+namespace Consuum.Clipboard
+{
+    public class MacClipboard : ITextReader
+    {
+        public List<string> GetLines()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
