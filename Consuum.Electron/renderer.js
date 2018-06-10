6d44352425fd3b5300358dd87d9422ae0f132379
Gian Lazzarini <gianlazzarini@gmail.com>
2018-06-10 12:49:28 -0700

    got edge-js example working

--- Consuum.Electron/renderer.js
+++ Consuum.Electron/renderer.js
@@ -2,29 +2,43 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 
-// Production Build
-const Moon = require("moonjs");
+//var edge = require('electron-edge-js');
+var edge = require('edge-js');
 
-// Development Build
-//const Moon = require("moonjs/dist/moon.js");
+var helloWorld = edge.func(function () {/*
+  async (input) => { 
+      return ".NET Welcomes " + input.ToString(); 
+  }
+*/});
 
-new Moon({
-    el: "#app",
-    data: {
-      msg: "Hello Moon!"
-    }
-  });
+helloWorld('JavaScript', function (error, result) {
+  if (error) throw error;
+  console.log(result);
+});
 
-  const app3 = new Moon({
-    el: "#app3",
-    data: {
-      msg: "Hello Moon!"
-    },
-    methods: {
-      changeMessage: function(msg) {
-        this.set('msg', msg);
-      }
-    }
-  });
+// // Production Build
+// const Moon = require("moonjs");
+
+// // Development Build
+// //const Moon = require("moonjs/dist/moon.js");
+
+// new Moon({
+//     el: "#app",
+//     data: {
+//       msg: "Hello Moon!"
+//     }
+//   });
+
+//   const app3 = new Moon({
+//     el: "#app3",
+//     data: {
+//       msg: "Hello Moon!"
+//     },
+//     methods: {
+//       changeMessage: function(msg) {
+//         this.set('msg', msg);
+//       }
+//     }
+//   });
   
-  app3.callMethod('changeMessage', ['New Message!']);
+//   app3.callMethod('changeMessage', [helloWorld]);
