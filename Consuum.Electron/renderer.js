3e8ad48e208babd81429717ce75ccef8d69c02b9
Gian Lazzarini <gianlazzarini@gmail.com>
2018-06-09 19:59:58 -0700

    Added Moon.js front end framework to electron layer

--- Consuum.Electron/renderer.js
+++ Consuum.Electron/renderer.js
@@ -1,3 +1,16 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+// Production Build
+const Moon = require("moonjs");
+
+// Development Build
+//const Moon = require("moonjs/dist/moon.js");
+
+new Moon({
+    el: "#app",
+    data: {
+      msg: "Hello Moon!"
+    }
+  });
