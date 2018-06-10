08ccc88acbf762b08440de8ee4871ca98285cd2e
Gian Lazzarini <gianlazzarini@gmail.com>
2018-06-10 16:57:18 -0700

    Experiemental async Task<object> method in EdgeJs.Program

--- Consuum.EdgeJs/Program.cs
+++ Consuum.EdgeJs/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Consuum.DiLibrary;
 using Consuum.Domain;
 
@@ -6,9 +7,19 @@ namespace Consuum.EdgeJs
 {
     public class Program
     {
-            public static PlatformEnum _platform = PlatformEnum.MacOS;
-            public static ITextReader _txtReader = Dependencies.CheckoutTextReader(_platform);
-            public static ITextToSpeech _tts = Dependencies.CheckoutTts(_platform);
+        public static PlatformEnum _platform = PlatformEnum.MacOS;
+        public static ITextReader _txtReader = Dependencies.CheckoutTextReader(_platform);
+        public static ITextToSpeech _tts = Dependencies.CheckoutTts(_platform);
 
+        public static async Task<object> TestClipboard()
+        {
+            return _txtReader.GetLines()[0];
+        }
+
+        // public async Task<object> Invoke(dynamic input)
+        // {
+        //     Person person = new Person();
+        //     return person;
+        // }
     }
 }
