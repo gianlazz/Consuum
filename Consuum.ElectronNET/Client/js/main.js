ac7bcf89fd9dd1f9ed61a5f41c2d50f61c8f1ba3
Gian Lazzarini <gianlazzarini@gmail.com>
2018-06-10 22:20:55 -0700

    Trying to get moonjs working

--- Consuum.ElectronNET/Client/js/main.js
+++ Consuum.ElectronNET/Client/js/main.js
@@ -1,5 +1,21 @@
 import '../css/site.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'jquery';
-import 'bootstrap';
+// import 'bootstrap/dist/css/bootstrap.css';
+// import 'jquery';
+// import 'bootstrap';
 import 'moonjs';
+
+const app1 = new Moon({
+    el: "#app1",
+    data: {
+      msg: "Hello Moon!"
+    }
+  });
+
+  const app2 = new Moon({
+    el: "#app2",
+    data: {
+      msg: "Hello Moon!"
+    }
+  });
+  
+  app2.set('msg', "Changed Message!");
