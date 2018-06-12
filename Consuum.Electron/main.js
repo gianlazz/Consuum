cbe0956e32395a5c53aac11a99a08973a3131a07
Gian Lazzarini <gianlazzarini@gmail.com>
2018-06-11 23:50:32 -0700

    Made electron window semi see through

--- Consuum.Electron/main.js
+++ Consuum.Electron/main.js
@@ -7,7 +7,7 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600, titleBarStyle: "hiddenInset", show: false})
+  mainWindow = new BrowserWindow({width: 800, height: 600, titleBarStyle: "hiddenInset", show: false, vibrancy: "ultra-dark"})
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
