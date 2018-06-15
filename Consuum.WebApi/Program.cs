d7de24be823ddf1fbd39f432f7f720dba489acba
Gian Lazzarini <gianlazzarini@gmail.com>
2018-06-14 20:59:45 -0700

    Copied over the program and startup for the webapi

--- Consuum.WebApi/Program.cs
+++ Consuum.WebApi/Program.cs
@@ -19,6 +19,7 @@ namespace Consuum.WebApi
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
                 .UseStartup<Startup>()
                 .Build();
     }
--- Consuum.WebApi/Startup.cs
+++ Consuum.WebApi/Startup.cs
@@ -23,18 +23,19 @@ namespace Consuum.WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            // Add framework services.
+            services.AddMvc().AddJsonOptions(options =>
+               {
+                   //return json format with Camel Case
+                   options.SerializerSettings.ContractResolver = new Newtonsoft.Json.Serialization.DefaultContractResolver();
+               });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
             app.UseMvc();
         }
     }
 }
+
