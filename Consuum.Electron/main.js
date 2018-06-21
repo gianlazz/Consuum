9f7023d4a7f9e684e4fc5c21a98520974c4b30ab
Gian Lazzarini <gianlazzarini@gmail.com>
2018-06-21 12:43:30 -0700

    removed problematic vibrancy electron options

--- Consuum.Electron/main.js
+++ Consuum.Electron/main.js
@@ -13,7 +13,7 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600, titleBarStyle: "hiddenInset", show: false, vibrancy: "ultra-dark"})
+  mainWindow = new BrowserWindow({width: 800, height: 600, titleBarStyle: "hiddenInset", show: false})
 
   // and load the index.html of the app.
   // mainWindow.loadURL(url.format({
